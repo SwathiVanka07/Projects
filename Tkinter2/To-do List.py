098e2a1afe7670581f3bc0d0c38668da7a7c339e
SwathiVanka07 <vankaswathi7@gmail.com>
2025-07-17 21:13:19 +0530

    Update To-do List.py

--- Tkinter2/To-do List.py
+++ Tkinter2/To-do List.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox, filedialog
 
 class TodoApp:
-    def __init__(self, root):
+    def __init__(self, root): 
         self.root = root
         self.root.title("To-Do List")
         self.root.geometry("400x450")
